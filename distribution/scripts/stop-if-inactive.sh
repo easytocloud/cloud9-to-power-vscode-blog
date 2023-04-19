3a9d7164082203a06e5ac8578887ac831a77e3a9
Erik Meinders <erik@easytocloud.com>
2023-04-19 17:07:21 +0200

    Minor change to activity detection

--- distribution/scripts/stop-if-inactive.sh
+++ distribution/scripts/stop-if-inactive.sh
@@ -22,7 +22,7 @@
 
 is_active()
 {
-    pgrep -f vfs-worker >/dev/null || pgrep -f vscode-server >/dev/null
+    pgrep -f vfs-worker >/dev/null || pgrep -fa vscode-server | grep -v -F 'shellIntegration-bash.sh' >/dev/null
 }
 
 is_shutting_down() {
